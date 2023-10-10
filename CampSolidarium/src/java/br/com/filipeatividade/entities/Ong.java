e4be081322c5b705cb69f82c61a6dd5f678b3ec1
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-10-10 20:03:10 -0300

    Fix: Entidade Ong

--- CampSolidarium/src/java/br/com/filipeatividade/entities/Ong.java
+++ CampSolidarium/src/java/br/com/filipeatividade/entities/Ong.java
@@ -11,4 +11,94 @@ package br.com.filipeatividade.entities;
  */
 public class Ong {
     
+    private Long id;
+    private String cnpj;
+    private String name;
+    private String login;
+    private String password;
+    private String email;
+    private String phone;
+    private String instagram;
+    
+    public Ong (){
+        
+    }
+
+    public Ong(Long id, String cnpj, String name, String login, String password, String email, String phone, String instagram) {
+        this.id = id;
+        this.cnpj = cnpj;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.instagram = instagram;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+    
+    
+    
 }
