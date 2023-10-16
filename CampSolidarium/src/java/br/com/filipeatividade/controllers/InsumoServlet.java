05d5edc31f7d7be7f6f17d60b9213dfe26ddd755
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-10-16 00:26:04 -0300

    Refatorando o Servlet de Insumo

--- CampSolidarium/src/java/br/com/filipeatividade/controllers/InsumoServlet.java
+++ CampSolidarium/src/java/br/com/filipeatividade/controllers/InsumoServlet.java
@@ -23,169 +23,159 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "InsumoServlet", urlPatterns = {"/InsumoServlet"})
 public class InsumoServlet extends HttpServlet {
 
-   
     @Override
-protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-    
-    String id = request.getParameter("id");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String id = request.getParameter("id");
 
         if (id != null) {
+            Insumo insumo = InsumoRepository.find(Long.parseLong(id));
 
-            Insumo i = InsumoRepository.find(Long.parseLong(id));
+            String operacao = request.getParameter("operacao");
 
-            String op = request.getParameter("operacao");
-
-            if (op != null && op.equals("edit")) {
-
-                response.setContentType("text/html;charset=UTF-8");
-                try (PrintWriter out = response.getWriter()) {
-                    /* TODO output your page here. You may use following sample code. */
-                    out.println("<!DOCTYPE html>");
-                    out.println("<html>");
-                    out.println("<head>");
-                    out.println("<title>Servlet UsuariosServlet</title>");
-                    out.println("</head>");
-                    out.println("<body>");
-                    out.println("<h1>Editar Usuário: " + i.getName() + "</h1>");
-
-                    out.println("<form method='post' action='InsumoServlet'>");
-
-                    out.println("<input type='hidden' name='codigo' value='" + i.getId() + "'/></br>");
-                    out.println("Name:<input type='text' name='name' value='" + i.getName() + "'/></br>");
-                    out.println("brand:<input type='text' name='brand' value='" + i.getBrand() + "'/></br>");
-                    out.println("Category:<input type='text' name='category' value='" + i.getCategory() + "'/></br>");
-                    out.println("Amount:<input type='text' name='qtd' value='" + i.getQtd() + "'/></br>");
-                    out.println("<input type='submit' value='editar'/></br>");
-                    out.println("</form>");
-
-                    out.println("<a href='InsumoServlet'>voltar</a>");
-                    out.println("</body>");
-                    out.println("</html>");
-                    return;
-
-                }
+            if ("edit".equals(operacao)) {
+                showEditForm(request, response, insumo);
+            } else if ("delete".equals(operacao)) {
+                deleteInsumo(request, response, insumo);
+            } else {
+                showDetails(response, insumo);
             }
-            else if(op.equals("delete")){
-                
-                InsumoRepository.delete(i);
-                
-                response.setContentType("text/html;charset=UTF-8");
-                try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Servlet UsuariosServlet</title>");
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>Insumo Deleted Successfully!</h1>");
-                out.println("<a href='InsumoServlet'>voltar</a>");
-                out.println("</body>");
-                out.println("</html>");
+        } else {
+            List<Insumo> insumos = InsumoRepository.findAll();
+            showInsumosList(response, insumos);
         }
-                
-            }
-
-            response.setContentType("text/html;charset=UTF-8");
-            try (PrintWriter out = response.getWriter()) {
-                /* TODO output your page here. You may use following sample code. */
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Servlet UsuariosServlet</title>");
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>Detalhes do usuário: " + i.getName() + "</h1>");
-                out.println("<h5>brand:" + i.getBrand() + "</h5>");
-                out.println("<h5>Category:" + i.getCategory() + "</h5>");
-                out.println("<h5>qunatidade:" + i.getQtd() + "</h5>");
-                out.println("<a href='InsumoServlet'>voltar</a>");
-                out.println("</body>");
-                out.println("</html>");
-
-                return;
-            }
-
-        }
-    
-    
-
-    List<Insumo> insumos = InsumoRepository.findAll();
-
-    response.setContentType("text/html;charset=UTF-8");
-    try (PrintWriter out = response.getWriter()) {
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Servlet RegisterInsumoServlet</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Insumos Cadastrados</h1>");
-        out.println("<a href='index.html'>home</a>");
-        out.println("<table border='1'>");
-        out.println("<tr><th>id</th><th>name</th><th>brand</th>"
-                + "<th>category</th><th>qtd</th><th>ações</th></tr>");
-
-        for (Insumo iAux : insumos) {
-            out.println("<tr>");
-            out.println("<td>" + iAux.getId() + "</td>");
-            out.println("<td>" + iAux.getName() + "</td>");
-            out.println("<td>" + iAux.getBrand() + "</td>");
-            out.println("<td>" + iAux.getCategory() + "</td>");
-            out.println("<td>" + iAux.getQtd() + "</td>");
-            out.println("<td><a href='InsumoServlet?id=" + iAux.getId() + "&operacao=detalhar'>detalhar</a> " +
-            "<a href='InsumoServlet?id=" + iAux.getId() + "&operacao=edit'>editar</a>" +
-            " <a href='InsumoServlet?id=" + iAux.getId() + "&operacao=delete'>deletar</a></td>");
-            out.println("</tr>");
-        }
-
-        out.println("</table>");
-        out.println("</body>");
-        out.println("</html>");
     }
-}
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Long id = Long.parseLong(request.getParameter("codigo"));
 
+        Insumo insumo = InsumoRepository.find(id);
 
-@Override
-protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-    
-    Long id = Long.parseLong(request.getParameter("codigo")); // Alteração aqui
+        insumo.setName(request.getParameter("name"));
+        insumo.setBrand(request.getParameter("brand"));
+        insumo.setCategory(request.getParameter("category"));
+        insumo.setQtd(Integer.parseInt(request.getParameter("qtd")));
 
-    Insumo i = InsumoRepository.find(id);
+        InsumoRepository.update(insumo);
 
-    i.setName(request.getParameter("name"));
-    i.setBrand(request.getParameter("brand"));
-    i.setCategory(request.getParameter("category"));
-    i.setQtd(Integer.parseInt(request.getParameter("qtd")));
-
-    InsumoRepository.update(i);
-
-    response.setContentType("text/html;charset=UTF-8");
-    try (PrintWriter out = response.getWriter()) {
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Insumo servlet</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Insumo Updated Successfully!</h1>");
-        out.println("<a href='InsumoServlet'>voltar</a>");
-        out.println("</body>");
-        out.println("</html>");
+        showUpdatedMessage(response);
     }
-}
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response, Insumo insumo) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Edit User</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Edit User: " + insumo.getName() + "</h1>");
+
+            out.println("<form method='post' action='InsumoServlet'>");
+            out.println("<input type='hidden' name='codigo' value='" + insumo.getId() + "'/></br>");
+            out.println("Name:<input type='text' name='name' value='" + insumo.getName() + "'/></br>");
+            out.println("Brand:<input type='text' name='brand' value='" + insumo.getBrand() + "'/></br>");
+            out.println("Category:<input type='text' name='category' value='" + insumo.getCategory() + "'/></br>");
+            out.println("Amount:<input type='text' name='qtd' value='" + insumo.getQtd() + "'/></br>");
+            out.println("<input type='submit' value='Edit'/></br>");
+            out.println("</form>");
+
+            out.println("<a href='InsumoServlet'>Back</a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    private void deleteInsumo(HttpServletRequest request, HttpServletResponse response, Insumo insumo) throws IOException {
+        InsumoRepository.delete(insumo);
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>User Deleted</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Insumo Deleted Successfully!</h1>");
+            out.println("<a href='InsumoServlet'>Back</a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    private void showDetails(HttpServletResponse response, Insumo insumo) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>User Details</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>User Details: " + insumo.getName() + "</h1>");
+            out.println("<h5>Brand:" + insumo.getBrand() + "</h5>");
+            out.println("<h5>Category:" + insumo.getCategory() + "</h5>");
+            out.println("<h5>Quantity:" + insumo.getQtd() + "</h5>");
+            out.println("<a href='InsumoServlet'>Back</a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    private void showInsumosList(HttpServletResponse response, List<Insumo> insumos) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Insumos List</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Registered Insumos</h1>");
+            out.println("<a href='index.html'>Home</a>");
+            out.println("<table border='1'>");
+            out.println("<tr><th>ID</th><th>Name</th><th>Brand</th>"
+                    + "<th>Category</th><th>Quantity</th><th>Actions</th></tr>");
+
+            for (Insumo iAux : insumos) {
+                out.println("<tr>");
+                out.println("<td>" + iAux.getId() + "</td>");
+                out.println("<td>" + iAux.getName() + "</td>");
+                out.println("<td>" + iAux.getBrand() + "</td>");
+                out.println("<td>" + iAux.getCategory() + "</td>");
+                out.println("<td>" + iAux.getQtd() + "</td>");
+                out.println("<td><a href='InsumoServlet?id=" + iAux.getId() + "&operacao=detalhar'>Details</a> " +
+                        "<a href='InsumoServlet?id=" + iAux.getId() + "&operacao=edit'>Edit</a>" +
+                        " <a href='InsumoServlet?id=" + iAux.getId() + "&operacao=delete'>Delete</a></td>");
+                out.println("</tr>");
+            }
+
+            out.println("</table>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    private void showUpdatedMessage(HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Insumo servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Insumo Updated Successfully!</h1>");
+            out.println("<a href='InsumoServlet'>Back</a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
 
     /**
      * Returns a short description of the servlet.
